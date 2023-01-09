f333287c2f256ea375441feae5e80868756c0f5e
Eimer Castro <Eimer.Castro@nordstrom.com>
2023-01-09 17:00:15 -0500

    Implenting custom exceptions and AccountEventSotre with EventStore.

---
+++ banking-account/account.cmd/src/main/java/com/banking/account/cmd/infrastructure/AccountEventStore.java
@@ -0,0 +1,59 @@
+package com.banking.account.cmd.infrastructure;
+
+import com.banking.account.cmd.domain.AccountAggregate;
+import com.banking.account.cmd.domain.EventStoreRepository;
+import com.banking.cqrs.core.events.BaseEvent;
+import com.banking.cqrs.core.events.EventModel;
+import com.banking.cqrs.core.exceptions.AggregateNotFoundException;
+import com.banking.cqrs.core.exceptions.ConcurrencyException;
+import com.banking.cqrs.core.infrastructure.EventStore;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@AllArgsConstructor
+@Service
+public class AccountEventStore implements EventStore {
+
+    private final EventStoreRepository eventStoreRepository;
+
+    @Override
+    public void saveEvent(String aggregateId, Iterable<BaseEvent> events, int expectedVersion) {
+        var eventStream = eventStoreRepository.findByAggregateIdentifier(aggregateId);
+        if (expectedVersion != -1 && eventStream.get(eventStream.size() - 1).getVersion() != expectedVersion) {
+            throw new ConcurrencyException();
+        }
+        var version = expectedVersion;
+        for (var event : events) {
+            version++;
+            event.setVersion(version);
+            var eventModel = EventModel.builder()
+                    .timeStamp(LocalDateTime.now())
+                    .aggregateIdentifier(aggregateId)
+                    .aggregateType(AccountAggregate.class.getTypeName())
+                    .version(version)
+                    .eventType(event.getClass().getTypeName())
+                    .eventData(event)
+                    .build();
+            var persistedEvent = eventStoreRepository.save(eventModel);
+
+            if (persistedEvent != null) {
+                // Produce a new event for Kafka
+            }
+        }
+    }
+
+    @Override
+    public List<BaseEvent> getEvents(String aggregateId) {
+        var eventStream = eventStoreRepository.findByAggregateIdentifier(aggregateId);
+        if (eventStream == null || eventStream.isEmpty()) {
+            throw new AggregateNotFoundException("Bank Account is wrong.");
+        }
+        return eventStream.stream()
+                .map(EventModel::getEventData)
+                .collect(Collectors.toList());
+    }
+}
--- cqrs-eventsourcing/cqrs.core/src/main/java/com/banking/cqrs/core/infrastructure/EventStore.java
+++ cqrs-eventsourcing/cqrs.core/src/main/java/com/banking/cqrs/core/infrastructure/EventStore.java
@@ -5,8 +5,6 @@ import com.banking.cqrs.core.events.BaseEvent;
 import java.util.List;
 
 public interface EventStore {
-
     void saveEvent(String aggregateId, Iterable<BaseEvent> events, int expectedVersion);
-
     List<BaseEvent> getEvents(String aggregateId);
 }
