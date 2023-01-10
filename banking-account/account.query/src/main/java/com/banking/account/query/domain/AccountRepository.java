87487fbb3d854af9cb573e5468d56f16f42a1034
Eimer Castro <Eimer.Castro@nordstrom.com>
2023-01-10 05:08:13 -0500

    Adding domain entities according to DDD.

---
+++ banking-account/account.query/src/main/java/com/banking/account/query/domain/AccountRepository.java
@@ -0,0 +1,17 @@
+package com.banking.account.query.domain;
+
+import com.banking.cqrs.core.domain.BaseEntity;
+import org.springframework.data.repository.CrudRepository;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
+
+public interface AccountRepository extends CrudRepository<BankAccount, String> {
+
+    Optional<BankAccount> findByAccountHolder(String accountHolder);
+
+    List<BaseEntity> findByBalanceGreaterThan(BigDecimal balance);
+
+    List<BaseEntity> findByBalanceLessThan(BigDecimal balance);
+}
---
+++ banking-account/account.query/src/main/java/com/banking/account/query/domain/BankAccount.java
@@ -0,0 +1,33 @@
+package com.banking.account.query.domain;
+
+import com.banking.account.common.dto.AccountType;
+import com.banking.cqrs.core.domain.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class BankAccount extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id", nullable = false)
+    private String id;
+    private String accountHolder;
+    private LocalDateTime creationDate;
+    private AccountType accountType;
+    private BigDecimal balance;
+
+}
