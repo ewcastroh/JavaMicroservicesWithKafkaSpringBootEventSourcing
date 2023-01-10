695973a598f6ab9d53f95048cf52ca7ff7cbfc4c
Eimer Castro <Eimer.Castro@nordstrom.com>
2023-01-10 09:36:57 -0500

    Registering query handlers.

--- banking-account/account.query/src/main/java/com/banking/account/query/QueryApplication.java
+++ banking-account/account.query/src/main/java/com/banking/account/query/QueryApplication.java
@@ -1,13 +1,32 @@
 package com.banking.account.query;
 
+import com.banking.account.query.api.queries.FindAccountByHolderQuery;
+import com.banking.account.query.api.queries.FindAccountByIdQuery;
+import com.banking.account.query.api.queries.FindAccountWithBalanceQuery;
+import com.banking.account.query.api.queries.FindAllAccountsQuery;
+import com.banking.account.query.api.queries.QueryHandler;
+import com.banking.cqrs.core.infrastructure.QueryDispatcher;
+import jakarta.annotation.PostConstruct;
+import lombok.AllArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@AllArgsConstructor
 @SpringBootApplication
 public class QueryApplication {
+
+	private final QueryDispatcher queryDispatcher;
+	private final QueryHandler queryHandler;
 
 	public static void main(String[] args) {
 		SpringApplication.run(QueryApplication.class, args);
 	}
 
+	@PostConstruct
+	public void registerHandlers() {
+		queryDispatcher.registerHandler(FindAllAccountsQuery.class, queryHandler::handle);
+		queryDispatcher.registerHandler(FindAccountByIdQuery.class, queryHandler::handle);
+		queryDispatcher.registerHandler(FindAccountByHolderQuery.class, queryHandler::handle);
+		queryDispatcher.registerHandler(FindAccountWithBalanceQuery.class, queryHandler::handle);
+	}
 }
